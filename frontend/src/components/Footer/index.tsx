3cd2f1f82c4bfaa6068ab5e04f37af9f92b4f0fa
Igor Souza <devsouza98@outlook.com>
2021-05-06 21:12:29 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+      
+            <footer className="footer mt-auto py-3 bg-dark">
+                <div className="container">
+                    <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/igor-souza-583b94131/" target="_blank" rel="noreferrer">Igor Souza</a></p>
+                    <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                        Evento promovido pela escola DevSuperior: <a href="https://instagram.com/" target="_blank" rel="noreferrer">@igor.souza98</a></small></p>
+                </div>
+            </footer>
+       
+    );
+}
+
+export default Footer;
